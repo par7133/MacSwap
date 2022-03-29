6b930e5f3c66ba2f9b51100ff3df7f479f22776d
Daniele Bonini (皮夕): WebDev and DevOps by lots of Sim.pli.city bits <my25mb@aol.com>
2022-03-30 00:19:47 +0200

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -3,20 +3,20 @@
 /**
  * Copyright 2021, 2024 5 Mode
  *
- * This file is part of 5 Cube.
+ * This file is part of MacSwap.
  *
- * 5 Cube is free software: you can redistribute it and/or modify
+ * MacSwap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * 5 Cube is distributed in the hope that it will be useful,
+ * MacSwap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.  
  * 
  * You should have received a copy of the GNU General Public License
- * along with 5 Cube. If not, see <https://www.gnu.org/licenses/>.
+ * along with MacSwap. If not, see <https://www.gnu.org/licenses/>.
  *
  * index.php
  * 
