d865f9ab4dce78d341ac9e4e9116b3fbe4b630a9
Daniele Bonini (皮夕): WebDev and DevOps by lots of Sim.pli.city bits <my25mb@aol.com>
2022-08-04 00:52:13 +0200

    Update home.js

--- Public/js/home.js
+++ Public/js/home.js
@@ -34,10 +34,10 @@ function showEncodedPassword() {
     $("#Password").addClass("emptyfield");
     return;  
   }
-  if ($("#Salt").val() === "") {
-    $("#Salt").addClass("emptyfield");
-    return;  
-  }	   	
+  //if ($("#Salt").val() === "") {
+  //$("#Salt").addClass("emptyfield");
+  //return;  
+  //}	   	
   passw = encryptSha2( $("#Password").val() + $("#Salt").val());
   msg = "Please set your hash in the config file with this value:";
   alert(msg + "\n\n" + passw);	
